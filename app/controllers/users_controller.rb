2ee89be2695a97459b7841e0cbdb349e94f5799a
rakuraku.musen <musen.rakuraku@gmail.com>
2018-09-24 09:23:32 +0000

    create_new_form_part1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+end
